5791444a34db86308b057fea1cb1975a3d11ff67
FabianSanchezD <fabiansanchezd@outlook.com>
2025-10-30 13:41:52 -0600

    implemented frontend

--- src/providers/StellarAccountProvider.tsx
+++ src/providers/StellarAccountProvider.tsx
@@ -1,66 +1,111 @@
+import { createContext, use, useCallback, useEffect, useState } from "react";
 import { IAccount } from "../interfaces/account";
-import { createContext, useCallback, useState, useMemo } from "react";
-import { use } from "react";
-import { getCurrentAccountFromStorage, saveCurrentAccount, getAccountFromStorage } from "../utils/localStorage";
+import {
+  getAccountFromStorage,
+  getCurrentAccountFromStorage,
+  saveCurrentAccount,
+} from "../utils/localStorage";
+import { UserRole } from "../interfaces/user-role";
+import { ICar } from "../interfaces/car";
 
 interface StellarContextType {
   currentAccount: string;
   hashId: string;
   setHashId: React.Dispatch<React.SetStateAction<string>>;
+  walletAddress: string;
+  setWalletAddress: (address: string) => void;
+  selectedRole: UserRole | null;
+  setSelectedRole: (role: UserRole | null) => void;
   setCurrentAccount: (name: string) => void;
   getAccount: (name: string) => IAccount | null;
   getCurrentAccountData: () => IAccount | null;
+  cars: ICar[];
+  setCars: React.Dispatch<React.SetStateAction<ICar[]>>;
 }
-  
-  const StellarAccountContext = createContext<StellarContextType | undefined>(
-    undefined
-  );
-  
-  export const useStellarAccounts = () => {
-    const context = use(StellarAccountContext);
-    if (context === undefined) {
-      throw new Error(
-        "useStellarAccounts must be used within a StellarAccountProvider"
-      );
-    }
-    return context;
-  };
-  
-  export const StellarAccountProvider: React.FC<{
-    children: React.ReactNode;
-  }> = ({ children }) => {
 
-    const [hashId, setHashId] = useState<string>("");
-    
-    const [currentAccount, setCurrentAccountState] = useState<string>(() =>
-      getCurrentAccountFromStorage()
+const StellarAccountContext = createContext<StellarContextType | undefined>(
+  undefined
+);
+
+export const useStellarAccounts = () => {
+  const context = use(StellarAccountContext);
+  if (context === undefined) {
+    throw new Error(
+      "useStellarAccounts must be used within a StellarAccountProvider"
     );
-  
-    const setCurrentAccount = useCallback((name: string) => {
-      setCurrentAccountState(name);
-      saveCurrentAccount(name);
-    }, []);
-  
-    const getAccount = useCallback((name: string): IAccount | null => {
-      return getAccountFromStorage(name);
-    }, []);
-  
-    const getCurrentAccountData = useCallback((): IAccount | null => {
-      if (!currentAccount) return null;
-      return getAccountFromStorage(currentAccount);
-    }, [currentAccount]);
-  
-    const value: StellarContextType = {
+  }
+  return context;
+};
+
+export const StellarAccountProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const [walletAddress, setWalletAddress] = useState<string>(() => {
+    return localStorage.getItem("wallet") || "";
+  });
+
+  const [currentAccount, setCurrentAccountState] = useState<string>(() =>
+    getCurrentAccountFromStorage()
+  );
+
+  const [hashId, setHashId] = useState<string>("");
+
+  const [selectedRole, setSelectedRole] = useState<UserRole | null>(() => {
+    const saved = localStorage.getItem("role");
+    return saved ? (saved as UserRole) : null;
+  });
+
+  const [cars, setCars] = useState<ICar[]>(() => {
+    const savedCars = localStorage.getItem("cars");
+    return savedCars ? (JSON.parse(savedCars) as ICar[]) : [];
+  });
+
+  const setCurrentAccount = useCallback((name: string) => {
+    setCurrentAccountState(name);
+    saveCurrentAccount(name);
+  }, []);
+
+  const getAccount = useCallback((name: string): IAccount | null => {
+    return getAccountFromStorage(name);
+  }, []);
+
+  const getCurrentAccountData = useCallback((): IAccount | null => {
+    if (!currentAccount) return null;
+    return getAccountFromStorage(currentAccount);
+  }, [currentAccount]);
+
+  useEffect(() => {
+    if (walletAddress) {
+      localStorage.setItem("wallet", walletAddress);
+    }
+  }, [walletAddress]);
+
+  useEffect(() => {
+    if (selectedRole) {
+      localStorage.setItem("role", selectedRole);
+    }
+  }, [selectedRole]);
+
+  useEffect(() => {
+    localStorage.setItem("cars", JSON.stringify(cars));
+  }, [cars]);
+
+  const value: StellarContextType = {
+    walletAddress,
     currentAccount,
     hashId,
     setHashId,
+    setWalletAddress,
     setCurrentAccount,
     getAccount,
+    selectedRole,
+    setSelectedRole,
     getCurrentAccountData,
+    cars,
+    setCars,
   };
-    return (
-      <StellarAccountContext.Provider value={value}>
-        {children}
-      </StellarAccountContext.Provider>
-    );
-  };
+
+  return (
+    <StellarAccountContext value={value}>{children}</StellarAccountContext>
+  );
+};
--- src/services/stellar.service.ts
+++ src/services/stellar.service.ts
@@ -1,298 +1,84 @@
-import { Asset, BASE_FEE, Horizon, Keypair, Operation, TransactionBuilder, xdr } from "@stellar/stellar-sdk";
-import { HORIZON_URL, STELLAR_FRIENDBOT_URL, STELLAR_NETWORK, STELLAR_NETWORK_PASSPHRASE } from "../utils/constants";
-import { IKeypair } from "../interfaces/keypair";
-import { IAccountBalanceResponse } from "../interfaces/balance";
-import { AccountBalance } from "../interfaces/account";
-
-type BuiltTransaction = ReturnType<TransactionBuilder["build"]>;
+import { contract, Horizon, TransactionBuilder } from "@stellar/stellar-sdk";
+import {
+  CONTRACT_ADDRESS,
+  STELLAR_NETWORK_PASSPHRASE,
+  HORIZON_URL,
+  SOROBAN_RPC_URL,
+} from "../utils/constants";
+// import { IKeypair } from "../interfaces/keypair";
+// import { IAccountBalanceResponse } from "../interfaces/balance";
+// import { AccountBalance } from "../interfaces/account";
+// import { ICreateClaimableBalanceResponse } from "../interfaces/claimable-balance";
 
 export class StellarService {
-  private server: Horizon.Server;
-  private network: string;
   private horizonUrl: string;
-  private friendBotUrl: string;
+  private server: Horizon.Server;
+  private rpcUrl: string;
   private networkPassphrase: string;
+  private contractAddress: string;
 
   constructor() {
-    this.network = STELLAR_NETWORK as string;
     this.horizonUrl = HORIZON_URL as string;
-    this.friendBotUrl = STELLAR_FRIENDBOT_URL as string;
+    this.rpcUrl = SOROBAN_RPC_URL as string;
     this.networkPassphrase = STELLAR_NETWORK_PASSPHRASE as string;
+    this.contractAddress = CONTRACT_ADDRESS as string;
 
     this.server = new Horizon.Server(this.horizonUrl, {
       allowHttp: true,
     });
   }
 
-  createAccount(): IKeypair {
-    const pair = Keypair.random();
-    return {
-        publicKey: pair.publicKey(),
-        secretKey: pair.secret(),
-    }
+  async buildClient<T = unknown>(publicKey: string): Promise<T> {
+    const client = await contract.Client.from({
+      contractId: this.contractAddress,
+      rpcUrl: this.rpcUrl,
+      networkPassphrase: this.networkPassphrase,
+      publicKey,
+    });
+
+    return client as T;
   }
 
-  async fundAccount(publicKey: string): Promise<boolean> {
+
+  async submitTransaction(xdr: string): Promise<string | undefined> {
     try {
-      if (this.network !== "testnet") {
-        throw new Error("Friendbot is only available on testnet");
-      }
+      const transaction = TransactionBuilder.fromXDR(
+        xdr,
+        this.networkPassphrase
+      );
+      const result = await this.server.submitTransaction(transaction);
 
-      const response = await fetch(`${this.friendBotUrl}?addr=${publicKey}`);
-
-      if (!response.ok) {
-        return false;
-      }
-
-      return true;
+      return result.hash;
     } catch (error: unknown) {
       console.error(error);
-      throw new Error(
-        `Error when funding account with Friendbot: ${error as string}`
-      );
-    }
-  }
 
-  private async getAccount(address: string): Promise<Horizon.AccountResponse> {
-    try {
-      return await this.server.loadAccount(address);
-    } catch (error) {
-      console.error(error);
-      throw new Error('Account not found');
-}}
+      const err = error as {
+        response?: {
+          data?: {
+            extras?: {
+              result_codes?: unknown;
+            };
+          };
+        };
+      };
 
-async getAccountBalance(publicKey: string): Promise<AccountBalance[]> {
-const account =
-  await this.getAccount(publicKey);
-
-return account.balances.map((b) => ({
-  assetCode:
-    b.asset_type === "native"
-      ? "XLM"
-      : (b as IAccountBalanceResponse).asset_code,
-
-  amount: b.balance,
-}));
-}
-
-private async loadAccount(address: string): Promise<Horizon.AccountResponse> {
-  try {
-    return await this.server.loadAccount(address);
-  } catch (error) {
-    console.error(error);
-    throw new Error("Account not found");
-  }
-}
-
-  private async checkTrustline(
-    assetIssuer: string,
-    assetCode: string,
-    destinationPubKey: string
-  ): Promise<boolean> {
-    const account = await this.loadAccount(destinationPubKey);
-    const balances = account.balances;
-    const assetToVerify = new Asset(assetCode, assetIssuer);
-
-    for (const balance of balances) {
-      if ("asset_code" in balance) {
-        const asset = new Asset(balance.asset_code, balance.asset_issuer);
-
-        if (asset.equals(assetToVerify)) {
-          return true;
-        }
-      }
-    }
-
-    return false;
-  }
-
-  private createTrustlineOperation(
-    asset: Asset,
-    source: string,
-    amount: string
-  ): xdr.Operation<Operation.ChangeTrust> {
-    const assetLimit = Number(amount) * 100;
-
-    return Operation.changeTrust({
-      asset,
-      source,
-      limit: assetLimit.toString(),
-    });
-  }
-
-  private getAsset(assetCode: string, assetIssuer: string): Asset {
-    if (assetCode === "XLM") {
-      return Asset.native();
-    }
-
-    return new Asset(assetCode, assetIssuer);
-  }
-
-  private transactionBuilder(
-    sourceAccount: Horizon.AccountResponse
-  ): TransactionBuilder {
-    return new TransactionBuilder(sourceAccount, {
-      fee: BASE_FEE,
-      networkPassphrase: this.networkPassphrase,
-    });
-  }
-
-  private createPaymentOperation(
-    amount: string,
-    asset: Asset,
-    destination: string
-  ): xdr.Operation<Operation.Payment> {
-    return Operation.payment({
-      destination,
-      asset,
-      amount,
-    });
-  }
-
-  private async submitTransaction(
-    transaction: BuiltTransaction
-  ): Promise<Horizon.HorizonApi.SubmitTransactionResponse> {
-    try {
-      return await this.server.submitTransaction(transaction);
-    } catch (error: any) {
-      console.error(error);
-
-      if (error?.response?.data?.extras?.result_codes) {
+      if (err.response?.data?.extras?.result_codes) {
         console.error(
-          "Transaction failed:",
-          error.response.data.extras.result_codes
+          "❌ Error en la transacción:",
+          err.response.data.extras.result_codes
         );
+      } else {
+        console.error("❌ Error general:", error);
       }
-
-      throw error;
     }
   }
-
-  private getAssetIssuer(
-    assetCode: string,
-    fallbackIssuer: string
-  ): string {
-    if (assetCode === "XLM") {
-      return fallbackIssuer;
-    }
-
-    return fallbackIssuer;
+  
+  environment(): { rpc: string; networkPassphrase: string } {
+    return {
+      rpc: this.rpcUrl,
+      networkPassphrase: this.networkPassphrase,
+    };
   }
-
-async payment(
-    senderPubKey: string,
-    senderSecret: string,
-    receiverPubKey: string,
-    receiverSecret: string,
-    amount: string,
-    assetCode: string
-  ): Promise<Horizon.HorizonApi.SubmitTransactionResponse> {
-    const sourceAccount = await this.loadAccount(senderPubKey);
-    const sourceKeypair = Keypair.fromSecret(senderSecret);
-    let hasTrustline: boolean = true;
-
-    const asset = this.getAsset(assetCode, receiverPubKey);
-    const transactionBuilder = this.transactionBuilder(sourceAccount);
-    
-    // if (asset.code !== "XLM" && asset.issuer !== receiverPubKey) {
-    //   hasTrustline = await this.checkTrustline(
-    //     receiverPubKey,
-    //     assetCode,
-    //     asset.issuer
-    //   );
-
-    //   if (!hasTrustline) {
-    //     const changeTrustOp = this.createTrustlineOperation(
-    //       asset,
-    //       receiverPubKey,
-    //       amount
-    //     );
-    //     transactionBuilder.addOperation(changeTrustOp);
-    //   }
-    // }
-
-    const paymentOperation = this.createPaymentOperation(
-      amount,
-      asset,
-      receiverPubKey
-    );
-
-    transactionBuilder.addOperation(paymentOperation);
-
-    const transaction = transactionBuilder.setTimeout(180).build();
-
-    transaction.sign(sourceKeypair);
-
-    // if (!hasTrustline) {
-    //   const recieveKeypair = Keypair.fromSecret(receiverSecret);
-    //   transaction.sign(recieveKeypair);
-    // }
-
-    return await this.submitTransaction(transaction);
-  }
-  async createAsset(
-    issuerSecret: string,
-    distributorSecret: string,
-    assetCode: string,
-    amount: string
-  ) {
-    const issuerKeys = Keypair.fromSecret(issuerSecret);
-    const distributorKeys = Keypair.fromSecret(distributorSecret);
-    const newAsset = new Asset(assetCode, issuerKeys.publicKey());
-    const assetLimit = Number(amount) * 100;
-
-    try {
-      const distributorAccount = await this.loadAccount(
-        distributorKeys.publicKey()
-      );
-
-      // Add new asset to the trustline of the distributor
-      // it is not necessary to initialize the asset
-      // with the trustline, the account can now receive the asset
-      const trustTransaction = new TransactionBuilder(distributorAccount, {
-        fee: BASE_FEE,
-        networkPassphrase: this.networkPassphrase,
-      })
-        .addOperation(Operation.changeTrust({
-            asset: newAsset,
-						source: distributorKeys.publicKey(),
-						limit: assetLimit.toString(),
-		    }))
-        .setTimeout(30)
-        .build();
-
-      trustTransaction.sign(distributorKeys);
-      await this.server.submitTransaction(trustTransaction);
-
-      const issuerAccount = await this.loadAccount(issuerKeys.publicKey());
-
-      // now here I issue the asset to the distributor account
-      // these tokens dont have a price, as they are not listed on a DEX
-      // basically these are minted tokens (or issued from the issuer to the distributor)
-      const issueTransaction = new TransactionBuilder(issuerAccount, {
-        fee: BASE_FEE,
-        networkPassphrase: this.networkPassphrase,
-      })
-        .addOperation(
-          Operation.payment({
-            destination: distributorKeys.publicKey(),
-            asset: newAsset,
-            amount,
-          })
-        )
-        .setTimeout(30)
-        .build();
-
-      issueTransaction.sign(issuerKeys);
-      const response= await this.server.submitTransaction(issueTransaction);
-
-      return response;
-    } catch (error) {
-      console.error("Error creating asset:", error);
-      throw error;
-    }
-  }
-
 }
 
 export const stellarService = new StellarService();
